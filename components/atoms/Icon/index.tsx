71b39cd0d8b4367b170f8e2b04aa7e6114f5d0a6
maintaein <daumdaum005@gmail.com>
2025-11-01 21:03:51 +0900

    fix: image 타입 에러를 수정하라

--- components/atoms/Icon/index.tsx
+++ components/atoms/Icon/index.tsx
@@ -15,7 +15,8 @@ type IconName =
   | 'search'
   | 'user'
   | 'settings'
-  | 'shuffle';
+  | 'shuffle'
+  | 'image';
 
 type IconSize = 'small' | 'medium' | 'large';
 
@@ -142,6 +143,14 @@ const icons: Record<IconName, React.ReactNode> = {
       strokeLinejoin="round"
       strokeWidth={2}
       d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
+    />
+  ),
+  image: (
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
     />
   ),
 };
